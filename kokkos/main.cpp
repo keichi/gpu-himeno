5900cd5014ca4c795b0e7cc167b0728760195999
Keichi Takahashi <hello@keichi.dev>
2024-08-30 15:15:15 +0900

    Refactor Kokkos version

--- kokkos/main.cpp
+++ kokkos/main.cpp
@@ -71,29 +71,29 @@
 #define MKMAX 1025
 #endif
 
-void initmt(Kokkos::View<float[MIMAX][MJMAX][MKMAX]> p,
-            Kokkos::View<float[MIMAX][MJMAX][MKMAX][4]> a,
-            Kokkos::View<float[MIMAX][MJMAX][MKMAX][3]> b,
-            Kokkos::View<float[MIMAX][MJMAX][MKMAX][3]> c,
-            Kokkos::View<float[MIMAX][MJMAX][MKMAX]> bnd,
-            Kokkos::View<float[MIMAX][MJMAX][MKMAX]> wrk1,
-            Kokkos::View<float[MIMAX][MJMAX][MKMAX]> wrk2)
+void initmt(Kokkos::View<float***, Kokkos::LayoutRight> p,
+            Kokkos::View<float****, Kokkos::LayoutRight> a,
+            Kokkos::View<float****, Kokkos::LayoutRight> b,
+            Kokkos::View<float****, Kokkos::LayoutRight> c,
+            Kokkos::View<float***, Kokkos::LayoutRight> bnd,
+            Kokkos::View<float***, Kokkos::LayoutRight> wrk1,
+            Kokkos::View<const float***, Kokkos::LayoutRight> wrk2)
 {
     Kokkos::parallel_for(
         "initmt1",
         Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0},
                                                {MIMAX, MJMAX, MKMAX}),
         KOKKOS_LAMBDA(int i, int j, int k) {
-            a(i, j, k, 0) = 0.0f;
-            a(i, j, k, 1) = 0.0f;
-            a(i, j, k, 2) = 0.0f;
-            a(i, j, k, 3) = 0.0f;
-            b(i, j, k, 0) = 0.0f;
-            b(i, j, k, 1) = 0.0f;
-            b(i, j, k, 2) = 0.0f;
-            c(i, j, k, 0) = 0.0f;
-            c(i, j, k, 1) = 0.0f;
-            c(i, j, k, 2) = 0.0f;
+            a(0, i, j, k) = 0.0f;
+            a(1, i, j, k) = 0.0f;
+            a(2, i, j, k) = 0.0f;
+            a(3, i, j, k) = 0.0f;
+            b(0, i, j, k) = 0.0f;
+            b(1, i, j, k) = 0.0f;
+            b(2, i, j, k) = 0.0f;
+            c(0, i, j, k) = 0.0f;
+            c(1, i, j, k) = 0.0f;
+            c(2, i, j, k) = 0.0f;
             p(i, j, k) = 0.0f;
             wrk1(i, j, k) = 0.0f;
             bnd(i, j, k) = 0.0f;
@@ -104,29 +104,30 @@ void initmt(Kokkos::View<float[MIMAX][MJMAX][MKMAX]> p,
         Kokkos::MDRangePolicy<Kokkos::Rank<3>>(
             {0, 0, 0}, {MIMAX - 1, MJMAX - 1, MKMAX - 1}),
         KOKKOS_LAMBDA(int i, int j, int k) {
-            a(i, j, k, 0) = 1.0f;
-            a(i, j, k, 1) = 1.0f;
-            a(i, j, k, 2) = 1.0f;
-            a(i, j, k, 3) = 1.0f / 6.0f;
-            b(i, j, k, 0) = 0.0f;
-            b(i, j, k, 1) = 0.0f;
-            b(i, j, k, 2) = 0.0f;
-            c(i, j, k, 0) = 1.0f;
-            c(i, j, k, 1) = 1.0f;
-            c(i, j, k, 2) = 1.0f;
+            a(0, i, j, k) = 1.0f;
+            a(1, i, j, k) = 1.0f;
+            a(2, i, j, k) = 1.0f;
+            a(3, i, j, k) = 1.0f / 6.0f;
+            b(0, i, j, k) = 0.0f;
+            b(1, i, j, k) = 0.0f;
+            b(2, i, j, k) = 0.0f;
+            c(0, i, j, k) = 1.0f;
+            c(1, i, j, k) = 1.0f;
+            c(2, i, j, k) = 1.0f;
             p(i, j, k) = (float)(i * i) / (float)((MIMAX - 2) * (MIMAX - 2));
             wrk1(i, j, k) = 0.0f;
             bnd(i, j, k) = 1.0f;
         });
 }
 
-float jacobi(int nn, Kokkos::View<float[MIMAX][MJMAX][MKMAX]> p,
-             Kokkos::View<float[MIMAX][MJMAX][MKMAX][4]> a,
-             Kokkos::View<float[MIMAX][MJMAX][MKMAX][3]> b,
-             Kokkos::View<float[MIMAX][MJMAX][MKMAX][3]> c,
-             Kokkos::View<float[MIMAX][MJMAX][MKMAX]> bnd,
-             Kokkos::View<float[MIMAX][MJMAX][MKMAX]> wrk1,
-             Kokkos::View<float[MIMAX][MJMAX][MKMAX]> wrk2)
+float jacobi(int nn,
+             Kokkos::View<float***, Kokkos::LayoutRight> p,
+             Kokkos::View<const float****, Kokkos::LayoutRight> a,
+             Kokkos::View<const float****, Kokkos::LayoutRight> b,
+             Kokkos::View<const float****, Kokkos::LayoutRight> c,
+             Kokkos::View<const float***, Kokkos::LayoutRight> bnd,
+             Kokkos::View<const float***, Kokkos::LayoutRight> wrk1,
+             Kokkos::View<float***, Kokkos::LayoutRight> wrk2)
 {
     float gosa;
     float omega = 0.8f;
@@ -136,24 +137,24 @@ float jacobi(int nn, Kokkos::View<float[MIMAX][MJMAX][MKMAX]> p,
 
         Kokkos::parallel_reduce(
             "jacobi1",
-            Kokkos::MDRangePolicy<Kokkos::Rank<3>>(
-                {1, 1, 1}, {MIMAX - 2, MJMAX - 2, MKMAX - 2}, {256, 4, 1}),
+            Kokkos::MDRangePolicy<Kokkos::Rank<3, Kokkos::Iterate::Left, Kokkos::Iterate::Right>>(
+                {1, 1, 1}, {MIMAX - 2, MJMAX - 2, MKMAX - 2}, {1, 16, 64}),
             KOKKOS_LAMBDA(int i, int j, int k, float &gosa) {
                 float s0 =
-                    a(i, j, k, 0) * p(i + 1, j, k) +
-                    a(i, j, k, 1) * p(i, j + 1, k) +
-                    a(i, j, k, 2) * p(i, j, k + 1) +
-                    b(i, j, k, 0) * (p(i + 1, j + 1, k) - p(i + 1, j - 1, k) -
+                    a(0, i, j, k) * p(i + 1, j, k) +
+                    a(1, i, j, k) * p(i, j + 1, k) +
+                    a(2, i, j, k) * p(i, j, k + 1) +
+                    b(0, i, j, k) * (p(i + 1, j + 1, k) - p(i + 1, j - 1, k) -
                                      p(i - 1, j + 1, k) + p(i - 1, j - 1, k)) +
-                    b(i, j, k, 1) * (p(i, j + 1, k + 1) - p(i, j - 1, k + 1) -
+                    b(1, i, j, k) * (p(i, j + 1, k + 1) - p(i, j - 1, k + 1) -
                                      p(i, j + 1, k - 1) + p(i, j - 1, k - 1)) +
-                    b(i, j, k, 2) * (p(i + 1, j, k + 1) - p(i - 1, j, k + 1) -
+                    b(2, i, j, k) * (p(i + 1, j, k + 1) - p(i - 1, j, k + 1) -
                                      p(i + 1, j, k - 1) + p(i - 1, j, k - 1)) +
-                    c(i, j, k, 0) * p(i - 1, j, k) +
-                    c(i, j, k, 1) * p(i, j - 1, k) +
-                    c(i, j, k, 2) * p(i, j, k - 1) + wrk1(i, j, k);
+                    c(0, i, j, k) * p(i - 1, j, k) +
+                    c(1, i, j, k) * p(i, j - 1, k) +
+                    c(2, i, j, k) * p(i, j, k - 1) + wrk1(i, j, k);
 
-                float ss = (s0 * a(i, j, k, 3) - p(i, j, k)) * bnd(i, j, k);
+                float ss = (s0 * a(3, i, j, k) - p(i, j, k)) * bnd(i, j, k);
 
                 gosa += ss * ss;
 
@@ -163,8 +164,8 @@ float jacobi(int nn, Kokkos::View<float[MIMAX][MJMAX][MKMAX]> p,
 
         Kokkos::parallel_for(
             "jacobi2",
-            Kokkos::MDRangePolicy<Kokkos::Rank<3>>(
-                {1, 1, 1}, {MIMAX - 2, MJMAX - 2, MKMAX - 2}, {256, 4, 1}),
+            Kokkos::MDRangePolicy<Kokkos::Rank<3, Kokkos::Iterate::Left, Kokkos::Iterate::Right>>(
+                {1, 1, 1}, {MIMAX - 2, MJMAX - 2, MKMAX - 2}, {1, 16, 64}),
             KOKKOS_LAMBDA(int i, int j, int k) { p(i, j, k) = wrk2(i, j, k); });
 
     } /* end n loop */
@@ -213,18 +214,19 @@ int main(int argc, char *argv[])
 
     Kokkos::ScopeGuard guard(argc, argv);
 
-    Kokkos::View<float[MIMAX][MJMAX][MKMAX]> p("p");
-    Kokkos::View<float[MIMAX][MJMAX][MKMAX][4]> a("a");
-    Kokkos::View<float[MIMAX][MJMAX][MKMAX][3]> b("b");
-    Kokkos::View<float[MIMAX][MJMAX][MKMAX][3]> c("c");
-    Kokkos::View<float[MIMAX][MJMAX][MKMAX]> bnd("bnd");
-    Kokkos::View<float[MIMAX][MJMAX][MKMAX]> wrk1("wrk1");
-    Kokkos::View<float[MIMAX][MJMAX][MKMAX]> wrk2("wrk2");
+    Kokkos::View<float***, Kokkos::LayoutRight> p("p", MIMAX, MJMAX, MKMAX);
+    Kokkos::View<float****, Kokkos::LayoutRight> a("a", 4, MIMAX, MJMAX, MKMAX);
+    Kokkos::View<float****, Kokkos::LayoutRight> b("b", 3, MIMAX, MJMAX, MKMAX);
+    Kokkos::View<float****, Kokkos::LayoutRight> c("c", 3, MIMAX, MJMAX, MKMAX);
+    Kokkos::View<float***, Kokkos::LayoutRight> bnd("bnd", MIMAX, MJMAX, MKMAX);
+    Kokkos::View<float***, Kokkos::LayoutRight> wrk1("wrk1", MIMAX, MJMAX, MKMAX);
+    Kokkos::View<float***, Kokkos::LayoutRight> wrk2("wrk2", MIMAX, MJMAX, MKMAX);
 
     /*
      *    Initializing matrixes
      */
     initmt(p, a, b, c, bnd, wrk1, wrk2);
+    Kokkos::fence();
     printf("mimax = %d mjmax = %d mkmax = %d\n", MIMAX, MJMAX, MKMAX);
     printf("imax = %d jmax = %d kmax =%d\n", MIMAX - 1, MJMAX - 1, MKMAX - 1);
 
@@ -234,6 +236,7 @@ int main(int argc, char *argv[])
 
     cpu0 = second();
     gosa = jacobi(nn, p, a, b, c, bnd, wrk1, wrk2);
+    Kokkos::fence();
     cpu1 = second();
     cpu = cpu1 - cpu0;
 
@@ -253,6 +256,7 @@ int main(int argc, char *argv[])
      */
     cpu0 = second();
     gosa = jacobi(nn, p, a, b, c, bnd, wrk1, wrk2);
+    Kokkos::fence();
     cpu1 = second();
 
     cpu = cpu1 - cpu0;
